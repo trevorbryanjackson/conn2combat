b24f12b81088b3ca33c8bc546a00295c5b674d90
trevorbryanjackson <trevorbryanjackson@gmail.com>
2024-12-21 16:34:12 -0600

    added limited output

--- extract_first_levels.py
+++ extract_first_levels.py
@@ -8,13 +8,10 @@ import sys
 import h5py
 import argparse
 
-######## CONFIG ########
-rois = ("Dentate", "dlPFC", "PMC")
-
 def options():
     parser = argparse.ArgumentParser(
     description="""
-                    This script is designed to parse .mat files generated from CONN first-level analyses (https://web.conn-toolbox.org/resources) 
+                    This script is designed to parse .mat files generated from CONN first-level ROI-to_ROI analyses (https://web.conn-toolbox.org/resources) 
                     and output them in a format that can be used by COMBAT (https://github.com/Jfortin1/ComBatHarmonization). 
 
                     Inputs are the CONN analysis directory, and an output directory where the outputs should be stored, and (optional) a list of ROIs of interest.
@@ -27,8 +24,8 @@ def options():
     parser.add_argument("-d", "--conn_directory", type=str, help="Path to the CONN analysis directory. Required.", required=True)
     parser.add_argument("-o", "--output_dir", type=str, help="Path to the directory where the outputs will be saved. Default is the current directory.", default=".")
     parser.add_argument("-r", "--rois", type=str, nargs='*',  help="List of regions of interest (ROIs) to export. Default is all ROIs.")
+    parser.add_argument("-l", "--limit", action="store_true",  help="Limits output to only ROI-to-ROI data (as opposed to ROIs-to-all including atlas and networks).")
     options = parser.parse_args()
-    print(f"Processing file: {options.file}")
     if options.conn_directory:
         print(f"Using connection directory: {options.conn_directory}")
     if options.output_dir:
@@ -38,11 +35,10 @@ def options():
     return options
 
 ######## CODE ########
-def run(data_dir, output_dir, rois):
+def run(data_dir, output_dir, rois, limit):
     pattern = 'resultsROI_Subject*_Condition001.mat'
     files = os.path.join(data_dir, pattern)
     all_data = {}
-
     for file in glob.glob(files):
         with h5py.File(file, 'r') as mat_data:
             names_dataset = mat_data['names']
@@ -52,7 +48,6 @@ def run(data_dir, output_dir, rois):
                 ascii_array = mat_data[ref][()]
                 name = ''.join(chr(code[0]) for code in ascii_array)
                 names.append(name)
-
             names2 = []
             for ref in names2_dataset[:, 0]:
                 ascii_array = mat_data[ref][()]
@@ -66,30 +61,27 @@ def run(data_dir, output_dir, rois):
                 )
             data = []
             for i, row_name in enumerate(names2):
+                if limit or rois:
+                    if row_name not in rois:
+                        continue
                 for j, col_name in enumerate(names):
+                    if limit:
+                        if col_name not in rois:
+                            continue
+                    if row_name == col_name:
+                        continue
                     data.append((f"{row_name}_{col_name}", Z[i, j]))
-
             df = pd.DataFrame(data, columns=["Name", "Score"])
-            if rois:
-                df = df[df["Name"].str.startswith(rois)]
-
             output_file = os.path.join(output_dir, f"{os.path.basename(file).split('.')[0]}_correlation_data_filtered.csv")
             df.to_csv(output_file, index=False)
             print(f"Saved filtered data: {output_file}")
-
-            # Add the data to all_data
-            subject_name = os.path.basename(file).split('_')[1]  # Extract subject number
+            subject_name = os.path.basename(file).split('_')[1] 
             all_data[subject_name] = df.set_index("Name")["Score"]
 
-    # Combine all subjects' data
     combined_df = pd.concat(all_data, axis=1)
     combined_df.columns = list(all_data.keys())
-
-    # Insert subject numbers as the first row
     subject_row = pd.DataFrame([combined_df.columns], columns=combined_df.columns)
     combined_df_with_header = pd.concat([subject_row, combined_df], ignore_index=True)
-
-    # Save the combined DataFrame as CSV
     combined_output_file = os.path.join(output_dir, "combat_correlation_data.csv")
     combined_df_with_header.to_csv(combined_output_file, header=False, index=False)
     print(f"Saved combined data: {combined_output_file}")
@@ -102,4 +94,6 @@ if __name__ == "__main__":
         print(f"Saving output to: {options.output_dir}")
     if options.rois:
         print(f"Processing the following ROIs: {', '.join(options.rois)}")
-    run(options.conn_directory, options.output_dir, options.rois)
+    if options.limit:
+        print(f"Limiting output to ROI-to-ROI data.")
+    run(options.conn_directory, options.output_dir, options.rois, options.limit)
